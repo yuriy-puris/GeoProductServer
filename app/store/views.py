a9a4a867893ed72fe70499be3eb9e4c820fc89d4
Yuriy Puris <ypuris@itera-research.com>
2019-12-13 17:57:32 +0200

    upd

---
+++ app/store/views.py
@@ -0,0 +1,97 @@
+import requests
+
+import asyncio
+import aiohttp
+
+import itertools
+
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Shop, Address, Coordinates
+from .serializers import AddressSerializer, CoordinatesSerializer
+from . import services
+from .services import service_address, service_coordinate, service_parse_location, service_search_parser
+
+class AddressView(APIView):
+    
+    def get(self, request, format=None):
+        # print(services.service_address)
+        # service_address.parser_address_moyo()
+        # service_address.parser_address_allo()
+        # service_address.parser_address_foxtrot()
+        
+        queryset = Address.objects.all()
+        serializer_class = AddressSerializer(queryset, many=True)
+        return Response(serializer_class.data)
+
+class CoordinatesView(APIView):
+
+    def get(self, request, format=None):
+        service_coordinate.parse_coordinates()
+
+        queryset = Coordinates.objects.all()
+        serializer_class = CoordinatesSerializer(queryset, many=True)
+        return Response(serializer_class.data)
+
+class ParseLocationView(APIView):
+
+    # def get(self, request):
+    #     print(request)
+    
+    def post(self, request):
+        # СДЕЛАТЬ ОБРАБОТКУ ОШИБОК
+        # ПРОВЕРИТЬ SHOP_LIST НА ДУБЛИКАТЫ
+        # ЕСЛИ ВВЕДЕНО НЕКОРРЕКТНОЕ НАЗВАНИЕ
+        # ЕСЛИ ПРИШЁЛ НЕУСПЕШНЫЙ ОТВЕТ ПРИ ЗАПРОСЕ
+        # ЕСЛИ ОШИБКА ВОЗНИКЛА ВО ВРЕМЯ ПАРСИНГА (НЕ ТОТ КЛАСС И Т.Д.)
+        # протестировать различные ключи запросов (Apple, Apple X, note 9)
+        # test_coord = { 
+        #     'lat': 50.0062302,
+        #     'lng': 36.2343553 
+        # }
+        #  dataSource = [
+        #     'Address', [ { 'address', 'shop' } ]
+        #     'Records': [ {'Products ...'} ]
+        # ]
+        search_query = request.data['search_query']
+        location = request.data['user_location']
+
+        nearest_address = service_parse_location.parse_location(location)
+        list_address = [i[0] for i in nearest_address]
+
+        shop_list = []
+        shop_requests = []
+
+        for point in list_address:
+            address = Address.objects.filter(id=point).values_list('shop_id_id')
+            shop_list.append(address[0][0])
+        
+        shop_list = list(set(shop_list))
+        
+        for shop_id in shop_list:
+            search_request = Shop.objects.filter(id=shop_id).values_list('search_request')
+            shop_info = [shop_id, search_request[0][0]]
+            shop_requests.append(tuple(shop_info))
+        
+        def call_url(shop):
+            request_query = shop[1] + search_query
+            response = service_search_parser.parse_shop(shop[0], request_query)
+            return response
+
+        def addition(Item):
+            return { 'id': Item }
+
+        futures = [call_url(shop) for shop in shop_requests]
+        flat_structure = list(itertools.chain.from_iterable(futures))
+        data_shop = list(map(addition, shop_list))
+        
+        main_data = {}
+        main_data['Records'] = flat_structure
+        main_data['Shops'] = data_shop
+
+        return Response(main_data)
+
